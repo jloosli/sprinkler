260d1db04719b92b99db9f7c5568585b1a8111c6
Jared Loosli <jloosli@gmail.com>
2013-08-20 22:16:40 -0600

    list to string

--- auto.py
+++ auto.py
@@ -104,7 +104,7 @@ class Scheduler:
         if start > datetime.datetime.now():
             delta = start - datetime.datetime.now()
             print(delta.total_seconds())
-            print("".join(zones))
+            print(",".join(["({0},{1})".format(x[0],x[1]) for x in zones]))
             log.info("Adding set that starts in {0}: {1}".format(delta.total_seconds(), "".join(zones)))
             waterset = {
                 "setId": uuid1(),  # Unique ID
